cc749b7a812edafe2e313e63dee44f91a895790c
Harsh Kumar <149566587+harshk9650@users.noreply.github.com>
2026-04-13 00:01:51 +0530

    Add files via upload

---
+++ First.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stdbool.h>
+
+int main(){
+   int sum = 0;
+   if(sum!=0){
+    printf("i'm non zero ");
+   }
+   else{
+    printf("i'm zero");
+   }
+
+    
+   
+    return 0;
+
+}
